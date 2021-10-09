522d5a7f3fa98f7263afe50f562cac338a3bdd9b
ninthy <ardaamemiss@gmail.com>
2021-10-10 02:25:39 +0300

    silly me

--- test.py
+++
@@ -1,6 +0,0 @@
-import ffpb
-from tqdm import tqdm
-import sys
-
-ffpb.main(argv=["-i", "https://vidroxy.com/eyJhbGciOiJIUzI1NiJ9.eyJzcnYiOjIxLCJkIjoxLCJuIjoianVqdXRzdS1rYWlzZW4iLCJzIjoiMSIsImUiOiIyMiIsInEiOiIxMDgwIn0.s0sBOFj_T5zVdCHsbVHe4OEDy5U_ZnV6qNRJgTc2DHk.m3u8", "test.mp4"], stream=sys.stderr, encoding=None, tqdm=tqdm)
-
