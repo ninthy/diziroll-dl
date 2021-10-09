47e16f4912ad26b7a4f05f6bb24c8e163c627a8e
ninthy <ardaamemiss@gmail.com>
2021-10-10 02:23:23 +0300

    big chunugs changes

--- index.py
+++ index.py
@@ -1,87 +1,88 @@
-from lib.diziroll import DiziRoll as dr
-from PyInquirer import prompt, print_json
-from lib.questions import Questions as q
+from src.questions import Questions as q
+from src.diziroll import DiziRoll as dr
+from PyInquirer import prompt
 from os import path
 import os
-from colorama import init, Fore, Back
+from rich import print as rprint
+import subprocess
+from rich.progress import Progress, BarColumn, SpinnerColumn, ProgressColumn, TimeRemainingColumn, DownloadColumn
 
-VERSION = 1.0
+VERSION = 1.9
 
-init()
 clear = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
 
 if __name__ == "__main__":
-    clear()
-    print(Fore.RED +  
-    f"""
- ____  _     _ ____       _ _   ____  _     
-|  _ \(_)___(_)  _ \ ___ | | | |  _ \| |    
-| | | | |_  / | |_) / _ \| | | | | | | |    
-| |_| | |/ /| |  _ < (_) | | | | |_| | |___ 
-|____/|_/___|_|_| \_\___/|_|_| |____/|_____| 
-                                             by {Fore.YELLOW + '@ninthy' + Fore.RED} 
-                                                 {Fore.GREEN + 'v' + str(VERSION) + Fore.RED}
-    """
-    + Fore.RESET)
-    
-    suggested_show = prompt(q.get_show_name_question())['show_name']
-    shows = dr.get_suggested_shows(suggested_show)
-    if not shows:
-        print("! Diziroll'a bağlanılamadı.")
-        exit()
-    
     while 1:
-        
-        
-        selected_show_answer = prompt(q.get_select_show_question([i["name"] for i in shows]))["selected_show"]
-        selected_show = list(filter(lambda x: x["name"] == selected_show_answer, shows))[0]
-        show = dr.get_show(selected_show["url"][1::])
-        if not show:
-            print("! Diziroll'a bağlanılamadı.")
-            break
-        folder_name = show['slug']
-        if not path.isdir(folder_name):
-            os.mkdir(folder_name)
+        clear()
+        rprint(f"""[red]
+     ____  _     _ ____       _ _   ____  _     
+    |  _ \(_)___(_)  _ \ ___ | | | |  _ \| |    
+    | | | | |_  / | |_) / _ \| | | | | | | |    
+    | |_| | |/ /| |  _ < (_) | | | | |_| | |___ 
+    |____/|_/___|_|_| \_\___/|_|_| |____/|_____| 
+                                                 [yellow]by github.com/ninthy[/yellow] 
+                                                     [green]v{str(VERSION)}[/green][/red]
+        """)
+        suggested_show = prompt(q.get_show_name_question(dr))['show_name']
+        shows = dr.get_suggested_shows(suggested_show)
+        if not shows:
+            rprint("[red]![/red] Diziroll'a bağlanılamadı.")
+            exit()
+        selected_type = prompt(q.get_select_type())["selected_type"]
+        is_downloadable = selected_type == q.to_be_downloaded
         
         while 1:
-            titles = [i["title"] for i in show["seasons"]]
-            titles.append(q.go_back)
-            
-            selected_season_answer = prompt(q.get_select_season_question(titles))["selected_season"]
-            print(selected_season_answer)
-            if selected_season_answer == q.go_back:
+            selected_show_answer = prompt(q.get_select_show_question([i["name"] for i in shows] + [q.go_back]))["selected_show"]
+            if selected_show_answer == q.go_back:
                 break
+            selected_show = list(filter(lambda x: x["name"] == selected_show_answer, shows))[0]
+            show = dr.get_show(selected_show["url"][1::])
+            if not show:
+                rprint("[red]![/red] Diziroll'a bağlanılamadı.")
+                break
+            folder_name = show['slug']
+            if not path.isdir(folder_name):
+                os.mkdir(folder_name)
 
-            selected_season = [i for i in show["seasons"] if i["title"] == selected_season_answer][0]
-            
-            selected_episode_answers = prompt(q.get_select_episodes_question(selected_season["episodes"]))["selected_episodes"]
-            print(len(selected_episode_answers))
-            total_episodes = selected_season["episodes"][:len(selected_episode_answers)]
-            
-            for idx, i in enumerate(total_episodes):
-                episode = dr.get_episode(i["id"])
-                if not episode:
-                    print("! Bir hata oluştu")
-                    continue
-                
-                episode_title = '{0}. Bölüm - {1}'.format(str(idx+1), i["title"])
-                sources = episode["sources"]
-                subtitles = episode["subtitles"]
-                subtitle_src = [i["src"] for i in subtitles if i["srclang"] == "tr"][0]
-                hd_source = [i for i in sources if i["res"] == "480"][0]
+            while 1:
+                titles = [i["title"] for i in show["seasons"]] + [q.go_back]
 
-                source_path = path.join(os.getcwd(), folder_name, episode_title)
-                dr.download_subtitle(subtitle_src, path=source_path)
-                dr.download_video(url=hd_source["src"], path=source_path)
-                print("! ", episode_title, "videosu indirildi.")
+
+                selected_season_answer = prompt(q.get_select_season_question(titles))["selected_season"]
+                if selected_season_answer == q.go_back:
+                    break
+                selected_season = [i for i in show["seasons"] if i["title"] == selected_season_answer][0]
+
+                selected_episode_answers = prompt(q.get_select_episodes_question(selected_season["episodes"]))["selected_episodes"]
+
+                total_episodes = selected_season["episodes"][:len(selected_episode_answers)]
+                selected_res_answer = prompt(q.get_select_res_question())["selected_res"]
+
+                episodes = dr.get_episodes_from_season(total_episodes, selected_res_answer, dr)
+                if not is_downloadable:
+                    p = subprocess.Popen("mpv "+' '.join([('--{ "' +i[0]+'" --sub-files="' + i[1] + '" --force-media-title="' +i[2]+ '" --term-playing-msg="'+i[2] + '" --title="'+i[2] +'" --}') for i in episodes]))
+                    rprint("[yellow]? [cyan]mpv[/cyan] açıldı, bölümleri oynatma listesinden seçebilir veya geçebilirsiniz.[/yellow]")
+                    exit()
+                    
+                with Progress(SpinnerColumn(), "[progress.description]{task.description}", BarColumn(style="yellow", complete_style="green", finished_style="red"), "[progress.percentage]{task.percentage:>3.0f}%", TimeRemainingColumn()) as progress:
+                    task = progress.add_task("[yellow]?[/yellow] Bölümler indiriliyor..", total=len(episodes))
+                    for episode in episodes:                    
+                        src, subtitle_src, episode_title = episode
+
+                        source_path = path.join(os.getcwd(), folder_name, episode_title)
+                        dr.download_subtitle(src, path=source_path)
+                        progress.update(task, description=f"{episode_title} altyazı indirildi.")
+                        progress.update(task, description=f"{episode_title} video indiriliyor.")
+                        dr.download_video(url=src, path=source_path, episode_title=episode_title)
+                        progress.update(task, description=f"{episode_title} video indirildi.", advance=1)
+                    progress.update(task, description="İndirme işlemi bitti: [yellow]%s" % source_path.split('\\')[:-1], completed=True)
+                    # print("! ", episode_title, "videosu indirildi.")
                 # open("test.json", "w", encoding="utf-8").write(json.dumps(episode))
-            
-            break
-        break
 
+                break
+            break
 """
 todo -
-    subtitles
-    error handling
-    
+    progress bar for downloading
+    mpv playlist media name
 """
---
+++ src/diziroll.py
@@ -0,0 +1,116 @@
+from json import dump
+from .questions import Questions as q
+import requests
+import subprocess
+from fake_useragent import UserAgent
+
+from rich import print as rprint
+
+from youtube_dl import YoutubeDL
+
+from rich.progress import Progress, BarColumn, SpinnerColumn, ProgressColumn, TimeRemainingColumn, DownloadColumn
+
+ua = UserAgent()
+endpoints = {
+    "search": "https://api.diziroll.com/v1/search/suggest",
+    "episode": "https://api.diziroll.com/v1/episode/source?episode_id={0}&debug=1",
+    "main": "https://api.diziroll.com/v1/series?slug={0}&fields%5B%5D=series.seasons&fields%5B%5D=series.seasons.episodes"
+}
+
+headers = {
+    "Referer": "https://www.diziroll.com/",
+    "sec-ch-ua": '" Not;A Brand";v="99", "Opera GX";v="79", "Chromium";v="93"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
+    "User-Agent": ua.random
+}  
+
+
+class DiziRoll:  
+    
+    @staticmethod
+    def get_episode(id: int):
+        response = requests.get(endpoints["episode"].format(str(id)), headers=headers)
+        try:
+            data = response.json()
+            if not data["status"]:
+                return None
+            return data["data"]
+        except Exception as e:
+            print(e)
+            return None
+
+    @staticmethod
+    def get_show(slug: str):
+        response = requests.get(endpoints["main"].format(slug), headers=headers)
+        try:
+            data = response.json()
+            
+            if not data["status"]:
+                return None
+            return data["data"]
+        except Exception as e:
+            print(e)
+            return None
+    
+    
+    @staticmethod
+    def download_subtitle(url: str, path: str):
+        query = f'youtube-dl "{url}" -o "{path}.vtt"'
+        process = subprocess.Popen(query, shell=True, stdout=subprocess.PIPE)
+        process.wait()
+        
+    @staticmethod
+    def download_video(url: str, path: str, episode_title: str):
+        query = f'youtube-dl "{url}" -o "{path}.vtt"'
+        process = subprocess.Popen(query, shell=True, stdout=subprocess.PIPE)
+        process.wait()
+
+    @staticmethod
+    def get_suggested_shows(name: str):
+        response = requests.post(endpoints["search"], data={"term": name}, headers=headers)
+        try:
+            data = response.json()
+            if not data["status"]:
+                return None
+            series = data["data"]["series"]
+            return series
+        except Exception as e:
+            print(e)
+            return None
+    @staticmethod
+    def get_episodes_from_season(total_episodes: list, selected_res_answer: str, dr):
+        episodes = []
+        with Progress(SpinnerColumn(), "[progress.description]{task.description}", BarColumn(style="yellow", complete_style="green", finished_style="red"), "[progress.percentage]{task.percentage:>3.0f}%", TimeRemainingColumn()) as progress:
+            task = progress.add_task("[yellow]?[/yellow] Bölümler çekiliyor..", total=len(total_episodes))
+            for idx, i in enumerate(total_episodes):
+                episode = dr.get_episode(i["id"])
+                if not episode:
+                    rprint("[red]![/red] Bir hata oluştu.")
+                    continue
+                
+                episode_title = '{0}. Bölüm - {1}'.format(str(idx+1), i["title"])
+                sources = episode["sources"]
+                subtitles = episode["subtitles"]
+                subtitle_src = [i["src"] for i in subtitles if i["srclang"] == "tr"][0]
+                
+                global source
+                for i in sources:
+                    if i["label"] == selected_res_answer:
+                        source = i
+                        break
+                    elif selected_res_answer == q.highest_res:
+                        highest_res = str(max([int(i["res"]) for i in sources]))
+                        if i["res"] == highest_res:
+                            source = i
+                            break
+                    elif selected_res_answer == q.lowest_res:
+                        lowest_res = str(min([int(i["res"]) for i in sources]))
+
+                        if i["res"] == lowest_res:
+                            source = i
+                            break                 
+                progress.update(task, description=f"[yellow]{episode_title}[/yellow] çekildi.", advance=1)
+                
+                episodes.append((source["src"], subtitle_src, episode_title))
+        return episodes
